0ed9c8a902738919e2d61bb58dfbf23cb93ca0d7
useekaw <useekaw@gmail.com>
2019-12-25 23:22:00 +0100

    Update config for extended bar

--- src/config.h
+++ src/config.h
@@ -6,13 +6,14 @@
 
 struct config cfg[] = {
     { "sysupdate", checkupdates, "%s ", "%s", 3600, 1 },
-    { "cpuperc", cpu_perc, "%s ", NULL, 1, 0 },
-    { "ram", ram, "%s ", NULL, 5, 0 },
     { "volume", volume, "%s ", "%s", 0, 0 },
     { "wifi", wifi, "%s ", "wlo1", 60, 0 },
-    { "battery", battery, "%s ", "BAT0", 60, 0 },
     // { datetime, " %s ", "%d.%m.", 5, 0, 0 },
     { "datetime", datetime, " %s", "%H:%M", 10, 0 },
+    { "datetime", datetime, ";L: %s", "%H:%M", 10, 0 },
+    { "cpuperc", cpu_perc, ";%s ", NULL, 1, 0 },
+    { "ram", ram, "%s", NULL, 5, 0 },
+    { "battery", battery, ";%s", "BAT0", 60, 0 },
 };
 
 #endif
